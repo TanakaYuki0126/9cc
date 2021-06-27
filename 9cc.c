02b019c74c724a4ffa81dcf2dfbccc4ee576f998
Tanaka Yuki <tnkyk009@gmail.com>
2021-06-27 18:11:20 +0900

    tokenizer if token have some spaces, its ok.

--- 9cc.c
+++ 9cc.c
@@ -1,33 +1,130 @@
+#include <ctype.h>
+#include <stdarg.h>
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
+typedef enum{
+    TK_RESERVED,
+    TK_NUM,
+    TK_EOF,
+} TokenKind;
+
+typedef struct Token Token;
+
+struct Token {
+    TokenKind kind;
+    Token *next;
+    int val;
+    char *str;
+};
+
+Token *token;
+
+void error(char *fmt, ...){
+    va_list ap;
+    va_start(ap, fmt);
+    vfprintf(stderr, fmt, ap);
+    fprintf(stderr, "\n");
+    exit(1);
+}
+
+
+//if next token is "op", put token forward and return true
+bool consume(char op){
+    if(token->kind != TK_RESERVED || token->str[0] != op)
+        return false;
+    token = token->next;
+    return true;
+}
+
+//if next token is op, put token forward.
+void expect(char op){
+    if(token->kind != TK_RESERVED || token->str[0] != op)
+      error("[function expect error]not '%c'", op);
+      
+    token = token->next;
+}
+
+
+//return the value if the token is number. and move the token forward.
+int expect_number(){
+    if(token->kind != TK_NUM) error("not a number");
+    int val = token->val;
+    token = token->next;
+    return val;
+}
+
+bool at_eof(){
+    return token->kind == TK_EOF;
+}    
+
+//create new token "tok" and connect to current token "cur"
+Token *new_token(TokenKind kind, Token *cur, char *str){
+    Token *tok = calloc(1, sizeof(Token));
+    tok->kind = kind;
+    tok->str = str;
+    cur->next = tok;
+    return tok;
+}
+
+
+//argv[1] will be tokenized by this funciton
+Token *tokenize(char *p){
+//define the first token named "head"
+    Token head;
+    head.next = NULL;
+    //cur is adress of current token
+    Token *cur = &head;
+    
+    while(*p){
+        if(isspace(*p)){
+            p++;
+            continue;
+        }
+        
+        if(*p == '+' || *p == '-'){
+            cur = new_token(TK_RESERVED, cur, p++);
+            continue;
+        }
+        
+        if(isdigit(*p)){
+            cur = new_token(TK_NUM, cur, p);
+            cur->val = strtol(p, &p, 10);
+            continue;
+        }
+        error("couldnt talknize");
+    }
+    new_token(TK_EOF, cur, p);
+    //head is empty token. so
+    return head.next;
+}
+
+
+    
 int main(int argc, char **argv){
     if(argc!= 2){
         fprintf(stderr, "input 2 argument");
         return 1;
     }
-    
-    char *p = argv[1];
+    //token: result of tokenized argv[1]
+    token = tokenize(argv[1]);
+   
     
     printf(".intel_syntax noprefix\n");
     printf(".global main\n");
     printf("main:\n");
-    printf("  mov rax, %ld\n", strtol(p, &p, 10));
-    while(*p){
-    	if(*p == '+'){
-    		p++;
-    		printf("    add rax, %ld\n", strtol(p, &p, 10));
+    printf("    mov rax, %d\n", expect_number());
+    while(!at_eof()){
+    	if(consume('+')){
+    		printf("    add rax, %d\n", expect_number());
     		continue;
     	}
     	
-    	if(*p == '-'){
-    		p++;
-    		printf("    sub rax, %ld\n", strtol(p, &p, 10));
-    		continue;
-    	}
+    	expect('-');
+    	printf("    sub rax, %d\n", expect_number());
     	
-    	fprintf(stderr, "unexpected character: '%c'\n", *p);
-    	return 1;
     }
     printf("    ret\n");
     return 0;
